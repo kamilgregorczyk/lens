5d5d16556fe9ec48616a8ed2f21771e080629b35
Sebastian Malton <sebastian@malton.name>
2021-12-06 14:00:52 -0500

    Ensure that all mentioned items in the extension API are exported
    - Require that all future changes to the extension API must also export
      everything
    
    - Add some documentation to some fields
    
    Signed-off-by: Sebastian Malton <sebastian@malton.name>

--- src/common/catalog-entities/kubernetes-cluster.ts
+++ src/common/catalog-entities/kubernetes-cluster.ts
@@ -129,10 +129,14 @@ export class KubernetesCluster<
 
     catalogCategoryRegistry
       .getCategoryForEntity<KubernetesClusterCategory>(this)
-      ?.emit("contextMenuOpen", this, context);
+      .emit("contextMenuOpen", this, context);
   }
 }
 
+/**
+ * The category for {@link KubernetesCluster}'s. Not constructable, use
+ * {@link kubernetesClusterCategory} to register event handlers.
+ */
 class KubernetesClusterCategory extends CatalogCategory {
   public readonly apiVersion = "catalog.k8slens.dev/v1alpha1";
   public readonly kind = "CatalogCategory";
@@ -154,6 +158,7 @@ class KubernetesClusterCategory extends CatalogCategory {
   };
 }
 
+export type { KubernetesClusterCategory };
 export const kubernetesClusterCategory = new KubernetesClusterCategory();
 
 catalogCategoryRegistry.add(kubernetesClusterCategory);
